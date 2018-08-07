d05e9be55e409f8d5ad373df132d98b0ceb5753b
Patricia Carter <patricia4194@gmail.com>
2018-08-07 23:16:31 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.length == 1 
+    return array[0]
+  elsif array.length == 2 
+    return array.join(" and ")
+  else 
+    last_item = array.pop
+    list = array.join(", ")
+    list << ", and " + last_item
+  end 
 end
