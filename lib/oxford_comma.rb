6e1604fc59a3a845b87f080377e28d699b1a6389
Patricia Carter <patricia4194@gmail.com>
2018-08-07 23:13:25 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
   if array.length == 1 
     return array[0]
   elsif array.length == 2 
-    return array.split(" and ")
+    return array.split(" , ")
   else 
     
   end 
