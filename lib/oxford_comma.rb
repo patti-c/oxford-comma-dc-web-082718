d7a112cf0f19e3820d9bc307340071bf4808849e
Patricia Carter <patricia4194@gmail.com>
2018-08-07 23:11:55 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,6 +2,8 @@ def oxford_comma(array)
   if array.length == 1 
     return array[0]
   elsif array.length == 2 
-    return array.split( and )
-  elsif 
+    return array.split(" and ")
+  else 
+    
+  end 
 end
